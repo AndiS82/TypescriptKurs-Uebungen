b8d17d87b18eeee96b090a7d08d273f39401c030
Andreas Schneider <xichtus@MacBook-Pro-von-Andreas.fritz.box>
2023-03-09 19:46:17 +0100

    uebung2

--- Week1/uebung 2/uebung2.js
+++ Week1/uebung 2/uebung2.js
@@ -5,4 +5,48 @@ const Concatenate = (first, second) => {
     return Uebungconcat;
 };
 console.log("ein ", "String");
+const Numberify = (numberify) => {
+    const number = Number(numberify);
+    return number;
+};
+console.log(Numberify("4"));
+const chainCreator = (toChain, multiplier) => {
+    let chainifyMe = "";
+    for (let i = 0; i < multiplier; i++) {
+        chainifyMe += toChain;
+    }
+    return chainifyMe;
+};
+console.log(chainCreator("test", 3));
+const isVowel = (letter) => {
+    if (letter === "a" ||
+        letter === "e" ||
+        letter === "i" ||
+        letter === "o" ||
+        letter === "u") {
+        return true;
+    }
+    else {
+        return false;
+    }
+};
+console.log(isVowel("a"));
+console.log(isVowel("k"));
+const weirdifyVowels = (word, number) => {
+    let result = word;
+    let times = 0;
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] === "a" ||
+            word[i] === "e" ||
+            word[i] === "i" ||
+            word[i] === "o" ||
+            word[i] === "u") {
+            result = result.slice(0, times) + word[i].repeat(number - 1) + result.slice(times);
+            times = times + number;
+        }
+        times++;
+    }
+    return result;
+};
+console.log(weirdifyVowels("ente", 5));
 //# sourceMappingURL=uebung2.js.map
--- Week1/uebung 2/uebung2.ts
+++ Week1/uebung 2/uebung2.ts
@@ -5,4 +5,59 @@ const Concatenate = (first:string, second:string) =>{
 
 console.log("ein ", "String")
 
+const Numberify = (numberify:string):number=>{
+    const number = Number(numberify)
+    return number
+}
+
+
+console.log(Numberify("4"))
+
+const chainCreator = (toChain:string, multiplier:number):string=>{
+    let chainifyMe: string =""
+    for (let i=0; i<multiplier; i++){ 
+        chainifyMe +=toChain}
+    return chainifyMe
+}
+
+console.log(chainCreator("test", 3))
+
+const isVowel =(letter:string):boolean=>{
+    if(
+        letter === "a" ||
+        letter === "e"||
+        letter ==="i"||
+        letter ==="o"||
+    letter === "u"    )
+    {
+        return true}
+    else {
+        return false
+    }
+}
+
+console.log(isVowel("a"))
+console.log(isVowel("k"))
+
+const weirdifyVowels = (word:string, number:number):string =>{
+let result: string = word
+let times:number =0
+for(let i=0; i < word.length; i++){
+    if(
+        word[i] === "a" ||
+        word[i] === "e" ||
+        word[i] === "i" ||
+        word[i] === "o" ||
+        word[i] === "u"
+    ){
+        result = result.slice(0, times) + word[i].repeat(number - 1) + result.slice(times)
+        times = times + number
+    }
+    times++
+}
+return result
+}
+
+console.log(weirdifyVowels("ente", 5))
+
 export{}
