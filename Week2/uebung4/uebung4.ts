1c70b13c7a534e88b1aef91309008994b3964d7a
Andreas Schneider <xichtus@MacBook-Pro-von-Andreas.fritz.box>
2023-03-16 22:48:09 +0100

    exercise 4

---
+++ Week2/uebung4/uebung4.ts
@@ -0,0 +1,26 @@
+type Player = {
+    Name: string;
+    lastLogin: Date;
+    ID: number;
+    Email: string;
+    PlayedGame: PlayedGame
+}
+
+type Admin = {
+    Name: string;
+    ID: number;
+    Email: string;
+    EmployeeID: number
+}
+
+
+type PlayedGame = {
+    Title: string;
+    Highscore: number
+}
+
+const getPlayerEmail = ({ Email }: { Email: Player["Email"]} | { Email: Admin["Email"] }=> {
+    console.log(Email)
+})
+
+getPlayerEmail(player1)
