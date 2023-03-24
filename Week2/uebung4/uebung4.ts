fd785502feeb6ac93e3edf87528ab8c15e32184c
Andreas Schneider <xichtus@MacBook-Pro-von-Andreas.fritz.box>
2023-03-24 21:01:33 +0100

    done

--- Week2/uebung4/uebung4.ts
+++ Week2/uebung4/uebung4.ts
@@ -104,7 +104,7 @@ function evaluateWinner(object1: PlayedGame, object2: PlayedGame): number{
 console.log(evaluateWinner(player1.Games, player2.Games))
 
 
-// Übung 6
+// Bonusaufgabe 1
 //screibe ein Leaderboard etc.
 
 const game1: PlayedGame = {
@@ -142,5 +142,67 @@ function LeaderboardEntry(x: PlayedGame[], name: string): PlayedGame[]{
 }
 
 console.log(LeaderboardEntry(gameArray, "FlappyDuck"))
+
+// Bonusaufgabe 2
+
+//erstelle einen Typen namens Highscore nach Vorgaben der Aufgabe
+
+type Highscore = {
+    Spielname: string,
+    Score: number,
+    Player: Player
+}
+
+// erstelle einen Typen namens Leaderboard nach Vorgaben der Aufgabe
+
+type Leaderboard = Highscore[];
+
+// erstelle eine Funktion, die einen Array von Playern nimmt und ein Leaderboard zurückgibt
+
+const player3: Player = {
+    Name: "Carina",
+    lastLogin: new Date(),
+    ID: 1912,
+    Email: "carina@carina.com",
+    Games: {
+        SpielName: "FlappyDuck",
+        HighScore:19121991
+    }
+}
+
+let LeaderboardFlappyDuck: Player[] = [];
+let LeaderboardSpaceInvaders: Player[] = [];
+
+const leaderboardAdvanced = (playerArray: Player[]):Leaderboard => {
+    playerArray.forEach(Player => {
+        if (
+            Player.Games.SpielName === "FlappyDuck"
+        )
+            LeaderboardFlappyDuck.push(Player)
+        if (
+            Player.Games. SpielName === "SpaceInvaders"
+        ) LeaderboardSpaceInvaders.push(Player)
+    })
+
+    const FlappyDuckTopPlayer: Player = LeaderboardFlappyDuck.sort((a, b) => b.Games.HighScore - a.Games.HighScore)[0];
+    const SpaceInvadersTopPlayer: Player = LeaderboardSpaceInvaders.sort((a, b) => b.Games.HighScore - a.Games.HighScore)[0]
+    
+    const UltimateHighscores: Highscore[] = [
+        {
+            Spielname: FlappyDuckTopPlayer.Games.SpielName,
+            Score: FlappyDuckTopPlayer.Games.HighScore,
+            Player: FlappyDuckTopPlayer
+        },
+        {
+            Spielname: SpaceInvadersTopPlayer.Games.SpielName,
+            Score: SpaceInvadersTopPlayer.Games.HighScore,
+            Player: SpaceInvadersTopPlayer
+        }
+    ]
+
+    return UltimateHighscores
+}
+
+console.log(leaderboardAdvanced([player1, player2, player3, Andi]))
 
 export {}
